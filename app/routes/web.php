80fe722dea8ca9a6e26e5aec5ebfca2f19fe0ac9
rocean <rocean74@gmail.com>
2024-03-05 18:16:44 +0200

    htaccess entry

--- app/routes/web.php
+++ app/routes/web.php
@@ -5,7 +5,7 @@ namespace apps4net\tasks\routes;
 use apps4net\tasks\libraries\Route;
 use apps4net\tasks\controllers\MainController;
 
-Route::get('index', [MainController::class, 'index']);
+Route::get('home', [MainController::class, 'index']);
 Route::get('groups', [MainController::class, 'groups']);
 Route::get('login', [MainController::class, 'login']);
 Route::get('register', [MainController::class, 'register']);
