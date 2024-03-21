33feea4111a04cd10047a4ded1a1bd21faabe629
rocean <rocean74@gmail.com>
2024-03-21 19:01:01 +0200

    Fix styling in teams card

--- app/components/team.php
+++ app/components/team.php
@@ -4,8 +4,10 @@ use apps4net\tasks\libraries\App;
 
 ?>
 
-<div class="card col-12 mt-3">
-    <h1 class="card-header"><?= $team?->getName() ?? 'Some team' ?></h1>
+<div class="card col-12 mt-3 px-0">
+    <div class="card-header">
+        <h1><?= $team?->getName() ?? 'Some team' ?></h1>
+    </div>
 
     <div class="card-body">
         <div id="usersInTeam<?= $team->getId() ?>" class="d-flex justify-content-center mt-3">
