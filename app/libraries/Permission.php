17e0a618a889223c6a58ce2b637f3afbbca360d8
rocean <rocean74@gmail.com>
2024-03-23 22:53:58 +0200

    Comments

--- app/libraries/Permission.php
+++ app/libraries/Permission.php
@@ -33,27 +33,37 @@ class Permission
         ],
     ];
 
+    /**
+     * Get the permission for the action
+     *
+     * @param string $action
+     * @return bool
+     */
     public static function getPermissionFor(string $action): bool
     {
+        // If the action is not in the permissions array, return true
         if(!array_key_exists($action, self::$permissions)) {
             return true;
         }
 
+        // Get the permissions for the action
         $permissionsForAction = self::$permissions[$action];
 
+        // If permissions has "all", return true
         if (in_array('all', $permissionsForAction)) {
             return true;
         }
-
 
         // If permissions has "login" and the user is not logged in, return false
         if (in_array('login', $permissionsForAction) && !isset($_SESSION['username'])) {
             return false;
         }
 
+        // If permissions has "login" and the user is logged in, check the role
         if (in_array('login', $permissionsForAction)) {
             $permissionRole = $permissionsForAction[1];
 
+            // If the user's role is less than the permission role, return false
             if ($_SESSION['role'] > $permissionRole) {
                 return false;
             }
