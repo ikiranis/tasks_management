484bef2ce2b8a8833f8cbccfaf3df46bfdce9264
rocean <rocean74@gmail.com>
2024-04-20 18:16:42 +0300

    Links for xml transform

--- app/views/teams.php
+++ app/views/teams.php
@@ -31,6 +31,11 @@ use apps4net\tasks\libraries\App;
             <?php App::component('createTeamForm'); ?>
 
             <?php App::component('exportToXMLForm'); ?>
+
+            <div class="d-flex justify-content-center mt-5">
+                <a href="xml" class="mx-3" target="_blank">XML transform σε HTML</a>
+                <a href="xml?enhance=true" class="mx-3" target="_blank">XML transform σε HTML με έξτρα στατιστικά</a>
+            </div>
         </div>
 
         <?php App::component('error'); ?>
